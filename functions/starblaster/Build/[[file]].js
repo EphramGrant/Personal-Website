21f77c32ad2d3b956b548b7e398ac08bfa5737b3
EphramGrant <ephram.grant@stellarusa.io>
2026-07-07 13:48:44 -0400

    Add onRequest function to handle asset requests

---
+++ functions/starblaster/Build/[[file]].js
@@ -0,0 +1,22 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const path = url.pathname;
+  const response = await context.env.ASSETS.fetch(context.request);
+  const headers = new Headers(response.headers);
+
+  if (path.endsWith('.wasm.gz')) {
+    headers.set('Content-Type', 'application/wasm');
+    headers.set('Content-Encoding', 'gzip');
+  } else if (path.endsWith('.framework.js.gz')) {
+    headers.set('Content-Type', 'application/javascript');
+    headers.set('Content-Encoding', 'gzip');
+  } else if (path.endsWith('.data.gz')) {
+    headers.set('Content-Type', 'application/octet-stream');
+    headers.set('Content-Encoding', 'gzip');
+  }
+
+  return new Response(response.body, {
+    status: response.status,
+    headers
+  });
+}
